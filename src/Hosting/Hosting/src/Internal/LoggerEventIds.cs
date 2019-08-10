fdd4ef5c7d8df01b1a2b814324cb72db43771d5c
Kahbazi <a.kahbazi@gmail.com>
2019-08-10 14:14:18 +0430

    Add event name for Microsoft.AspNetCore.Hosting

--- src/Hosting/Hosting/src/Internal/LoggerEventIds.cs
+++ src/Hosting/Hosting/src/Internal/LoggerEventIds.cs
@@ -1,21 +1,23 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.Extensions.Logging;
+
 namespace Microsoft.AspNetCore.Hosting
 {
     internal static class LoggerEventIds
     {
-        public const int RequestStarting = 1;
-        public const int RequestFinished = 2;
-        public const int Starting = 3;
-        public const int Started = 4;
-        public const int Shutdown = 5;
-        public const int ApplicationStartupException = 6;
-        public const int ApplicationStoppingException = 7;
-        public const int ApplicationStoppedException = 8;
-        public const int HostedServiceStartException = 9;
-        public const int HostedServiceStopException = 10;
-        public const int HostingStartupAssemblyException = 11;
-        public const int ServerShutdownException = 12;
+        public const EventId RequestStarting = new EventId(1, "RequestStarting");
+        public const EventId RequestFinished = new EventId(2, "RequestFinished");
+        public const EventId Starting = new EventId(3, "Starting");
+        public const EventId Started = new EventId(4, "Started");
+        public const EventId Shutdown = new EventId(5, "Shutdown");
+        public const EventId ApplicationStartupException = new EventId(6, "ApplicationStartupException");
+        public const EventId ApplicationStoppingException = new EventId(7, "ApplicationStoppingException");
+        public const EventId ApplicationStoppedException = new EventId(8, "ApplicationStoppedException");
+        public const EventId HostedServiceStartException = new EventId(9, "HostedServiceStartException");
+        public const EventId HostedServiceStopException = new EventId(10, "HostedServiceStopException");
+        public const EventId HostingStartupAssemblyException = new EventId(11, "HostingStartupAssemblyException");
+        public const EventId ServerShutdownException = new EventId(12, "ServerShutdownException");
     }
 }
